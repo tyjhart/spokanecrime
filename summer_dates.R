47914ab8354602cf6218f4c4cbeea8b072c1b4c3
Tyler Hart <tyler@manitonetworks.com>
2019-04-11 19:02:21 -0400

    Relocate summer dates to separate file

---
+++ summer_dates.R
@@ -0,0 +1,10 @@
+# Beginning, end dates for summer
+summer_start <- c("2014-06-21", "2015-06-20", "2016-06-20", "2017-06-20", "2018-06-21")
+summer_end <-   c("2014-09-22", "2015-09-23", "2016-09-22", "2017-09-22", "2018-09-22")
+summer_year <-  c(2014, 2015, 2016, 2017, 2018)
+
+summer_dates <- data.frame(
+  summer_start = as.Date(summer_start), 
+  summer_end = as.Date(summer_end), 
+  year = summer_year
+)
