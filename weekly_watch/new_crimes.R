9dadfb7cff135fda96d51f2ba517e6605f64611b
Tyler Hart <tyler@manitonetworks.com>
2019-04-23 00:48:51 -0400

    Fixes and updates

--- weekly_watch/new_crimes.R
+++ weekly_watch/new_crimes.R
@@ -3,26 +3,35 @@ df.crimes <- data.frame(stringsAsFactors = FALSE)
 
 text_file_list <- list.files(path = "./text_files/", pattern = "\\.txt$", full.names = TRUE)
 
+# District
+police_districts <- c("p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "spa", "spb", "spc", "spd", "oth", "unk")
+
+# Line header A/C/U
+line_start_letter <- c("a", "c", "u")
+
 for (file_name in text_file_list) {
   text.crime <- readLines(file_name)
   
-  for (line in text.crime) {
-    line <- str_to_lower(line) # Lowercase for easier comparison
+  line_parsed <- foreach(
+    line_num = 1:length(text.crime), 
+    .combine = rbind,
+    .packages = c("stringr", "data.table", "tidyverse")
+    ) %dopar% {
+    line <- str_to_lower(text.crime[line_num]) # Lowercase for easier comparison
     exploded_line <- strsplit(line, " ") # Explode the text line
+    line_length <- length(exploded_line[[1]]) # Length of exploded elements
     
     # A/C (or U)
     ifelse(
-      exploded_line[[1]][1] %in% c("a", "c", "u"),
+      exploded_line[[1]][1] %in% line_start_letter,
       ac <- exploded_line[[1]][1],
       {
         exploded_line[[1]] <- shift(exploded_line[[1]], n=1, type="lag")
-        ac <- "U"
-        }
+        ac <- "u"
+      }
     )
     
-    # District
-    police_districts <- c("p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "spa", "spb", "spc", "spd", "oth", "unk")
-    
+    # Police districts
     ifelse(
       tail(exploded_line[[1]], n=1) %in% police_districts,
       district <- tail(exploded_line[[1]], n=1),
@@ -32,52 +41,19 @@ for (file_name in text_file_list) {
       }
     )
     
-    # Get total line length
-    line_length <- length(exploded_line[[1]]) 
-      
     # Date
-    date <- as.Date(exploded_line[[1]][line_length-1], "%m/%d/%Y") # Offense Date
+    raw_date <- exploded_line[[1]][line_length-1]
+    epoch_days_date <- as.Date(raw_date, "%m/%d/%Y") # Offense Date
     
-    # Paste offense name and address back together
-    offense_location_string <- paste(
-      exploded_line[[1]][2:as.numeric(line_length - 2)], 
-      collapse = " "
-    )
-    
-    # Match offense name to defined vector
-    # offense <- ifelse(
-    #   TRUE %in% str_detect(offense_location_string, offense_list) == TRUE,
-    #   offense_list[str_detect(offense_location_string, offense_list) == TRUE],
-    #   "unknown"
-    # )
-    
-    offense <- ifelse(
-      TRUE %in% str_detect(offense_location_string, df.offenses$string),
-      df.offenses$summary[str_detect(offense_location_string, df.offenses$string) == TRUE],
-      "unknown"
-    )
-    
-    # Match degree to defined vector
-    degree <- ifelse(
-      TRUE %in% str_detect(offense_location_string, df.degrees$name.degree) == TRUE,
-      df.degrees$number.degree[str_detect(offense_location_string, df.degrees$name.degree) == TRUE],
-      NA
-    )
-
-    offense_line <- data.frame(
-      ac = ac, 
-      date = date,
-      offense = offense,
-      degree = degree,
-      district = district,
-      location = offense_location_string,
-      line = line,
-      stringsAsFactors = FALSE
-    )
-    
-    df.crimes <- rbind(df.crimes, offense_line)
+    # Return parsed line
+    return(c(ac, district, epoch_days_date, line))
   }
+  
+  df.crimes <- rbind(df.crimes, data.frame(line_parsed, stringsAsFactors = FALSE))
 }
 
-# Isolate locations
-df.crimes$location <- str_replace_all(df.crimes$location, paste(offense_names, collapse = "|"), "")
+colnames(df.crimes) <- c("ac", "district", "date", "line")
+
+df.crimes$ac <- factor(df.crimes$ac, levels = line_start_letter)
+df.crimes$district <- factor(df.crimes$district, levels = police_districts)
+df.crimes$date <- as.Date(as.integer(df.crimes$date), origin = "1970-01-01")
