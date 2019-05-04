04366342e2189557dd1e2fec753412ef0d926c82
Tyler Hart <tyler@manitonetworks.com>
2019-05-04 16:56:40 -0400

    Initial CSV file formatting

---
+++ weekly_watch/csv_files/csv_import.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(readr)
+
+files = list.files(pattern="*.csv")
+df.crimes = lapply(files, read_csv) %>% bind_rows()
+
+df.crimes$date <- as.Date(df.crimes$date, "%m/%d/%Y")
+df.crimes$ac <- as.factor(toupper(df.crimes$ac))
+df.crimes$district <- as.factor(toupper(df.crimes$district))
+df.crimes$offense <- as.factor(tolower(df.crimes$offense))
+
+# Generate address list CSV
+# df.crimes$city <- "Spokane"
+# df.crimes$state <- "WA"
+# df.crimes$zip <- ""
+# 
+# df.crimes %>% 
+#   filter(!is.na(address)) %>% 
+#   distinct() %>%
+#   select(address, city, state, zip) %>%
+#   write.csv(., 'addresses.csv')
+
+table(df.crimes$district)
+table(df.crimes$ac)
+table(df.crimes$offense)
