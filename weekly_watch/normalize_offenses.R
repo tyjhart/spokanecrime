2002653dce554576dce51a46e86b63895f980d32
Tyler Hart <tyler@manitonetworks.com>
2019-06-23 13:46:03 -0400

    No longer used

--- weekly_watch/normalize_offenses.R
+++
@@ -1,2 +0,0 @@
-# True-up offenses
-df.crimes$offense[which(df.crimes$offense == "Vehicle Theft")] <- "Theft (Vehicle-Related)"
--- weekly_watch/vehicle_offense.R
+++
@@ -1,8 +0,0 @@
-# Motor vehicle involvement
-motor_veh_terms <- c("vehicle", "vehicular", "veh-")
-
-df.crimes$motor_vehicle_involved <- ifelse(
-  TRUE %in% str_detect(df.crimes$offense, motor_veh_terms) == TRUE,
-  1,
-  0
-)
