f029d519e4b1d4312be91eef112af7cf2a3aabae
Tyler Hart <tyler@manitonetworks.com>
2020-03-30 14:54:39 -0700

    Categorize by violent, non-violent offense

---
+++ weekly_watch/categorize_violence.R
@@ -0,0 +1,6 @@
+violent_crimes <- c("ASSAULT","HOMICIDE","ROBBERY","RAPE","MURDER")
+
+df.crimes$violence <- NA
+
+df.crimes[which(df.crimes$category %in% violent_crimes),]$violence <- "Violent"
+df.crimes[which(!df.crimes$category %in% violent_crimes),]$violence <- "Non-Violent"
