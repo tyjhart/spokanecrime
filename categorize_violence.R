997aa61dfde02688f267bae7b8eea5b70fd47cf0
Tyler Hart <tyler@manitonetworks.com>
2022-01-13 16:02:35 -0800

    Add Drive-By shootings and a legal ref

--- categorize_violence.R
+++ categorize_violence.R
@@ -1,4 +1,7 @@
-violent_crimes <- c("ASSAULT","HOMICIDE","ROBBERY","RAPE","MURDER")
+### Categorize crimes by violence ###
+# Reference https://app.leg.wa.gov/rcw/default.aspx?cite=9.94A.030
+
+violent_crimes <- c("ASSAULT","HOMICIDE","ROBBERY","RAPE","MURDER","DRIVE-BY")
 
 df.crimes$violence <- NA
 
