573a149e3366686ff26ed6e4c645d986778a209e
Tyler Hart <tyler@manitonetworks.com>
2022-02-07 16:26:18 -0800

    Move some code around

--- lunar_seasons.R
+++ lunar_seasons.R
@@ -5,3 +5,9 @@ df.crimes$lunar_phase <- as.factor(df.crimes$lunar_phase)
 # Season
 df.crimes$season <- terrestrial.season(df.crimes$date)
 df.crimes$season <- as.factor(df.crimes$season)
+
+# Moon phase abbreviations
+levels(df.crimes$lunar_phase)[levels(df.crimes$lunar_phase)=="Waning gibbous"] <- "Waning Gibb."
+levels(df.crimes$lunar_phase)[levels(df.crimes$lunar_phase)=="Waning crescent"] <- "Waning Cres."
+levels(df.crimes$lunar_phase)[levels(df.crimes$lunar_phase)=="Waxing crescent"] <- "Waxing Cres."
+levels(df.crimes$lunar_phase)[levels(df.crimes$lunar_phase)=="Waxing gibbous"] <- "Waxing Gibb."
